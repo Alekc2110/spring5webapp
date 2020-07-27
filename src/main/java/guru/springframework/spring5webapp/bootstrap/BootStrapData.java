6de6b73a8bad783d9e19901117d7e22ead0b8fa3
Alekc2110 <0504268686@ukr.net>
2020-07-28 00:32:40 +0300

    added publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,9 +1,13 @@
 package guru.springframework.spring5webapp.bootstrap;
 
+import guru.springframework.spring5webapp.domain.Address;
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AddressRepository;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +15,14 @@ import org.springframework.stereotype.Component;
 public class BootStrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+    private final AddressRepository addressRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository, AddressRepository addressRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+        this.addressRepository = addressRepository;
     }
 
 
@@ -32,7 +40,15 @@ public class BootStrapData implements CommandLineRunner {
         Book noEJB = new Book("J2EE Dev without EJB", "6465");
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+
+
+        Address pub1Address = new Address("Dnnc.str, 55","New York", "New York", 6552L);
+        addressRepository.save(pub1Address);
+        Publisher publisher1 = new Publisher("John", pub1Address);
+        publisherRepository.save(publisher1);
+
         System.out.println("Started");
         System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("First Publisher: " + publisherRepository.findAll());
     }
 }
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Address.java
@@ -0,0 +1,93 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String address_line;
+    private String city;
+    private String state;
+    private Long zip;
+
+    public Address() {
+    }
+
+    public Address(String address_line, String city, String state, Long zip) {
+        this.address_line = address_line;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress_line() {
+        return address_line;
+    }
+
+    public void setAddress_line(String address_line) {
+        this.address_line = address_line;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getZip() {
+        return zip;
+    }
+
+    public void setZip(Long zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(id, address.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", address_line='" + address_line + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                '}';
+    }
+}
